7f52950670e56bf8fbdd37825892d5abbdc03ce2
Anders Kristoffersson <anderskristo@gmail.com>
2021-07-08 00:08:38 +0200

    searching game test data

--- content.js
+++ content.js
@@ -1,6 +1,18 @@
 const App = {
   init: () => {
-    console.log('init', $('body'));
+    const location = window.location.href;
+    console.log(location);
+    var res = location.split('/');
+    var pos = res.indexOf('match');
+
+    if (res.indexOf('match')) {
+      App.awaitingGame(res[pos+1]);
+    }
+  },
+  awaitingGame: (matchId) => {
+    // When waiting game, a regular matchid is created... so here we can do magic.
+    // Get all players... get recent games... count win streak... or whatever..
+    console.log(matchId);
   },
 }
 
